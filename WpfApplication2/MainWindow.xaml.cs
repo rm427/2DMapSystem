925e31b09cc936d15f99e62b794e07723a4ead7d
rm427 <rupertmitchell1@gmail.com>
2015-11-08 09:14:06 +0000

    New CSV save/load functions

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,8 +51,14 @@ namespace WpfApplication2
         {
             InitializeComponent();
 
-            DisplayedTiles = new Location[XDimension, YDimension];
-            DisplayedTiles = GridFunctions.GenerateTileArray(XDimension, YDimension);
+            //////// Below is generating and saving a location array.
+            //////// Currently it is set up to load rather than save.
+            //////// Delete the below comment prefixes and add them to LoadCSV to change that.
+            /// DisplayedTiles = new Location[XDimension, YDimension];
+            /// DisplayedTiles = GridFunctions.GenerateTileArray(XDimension, YDimension);
+            /// SaveCSV(DisplayedTiles, "locations.csv", XDimension, YDimension);
+
+            DisplayedTiles = LoadCSV("locations.csv");
 
             /// Create player sprite
             MainCanvas.Children.Add(PlayerSprite = new Image { Height = TileHeight, Width = TileWidth });
@@ -251,6 +258,65 @@ namespace WpfApplication2
             }
             e.Handled = true;
         }
+
+        public void SaveCSV(Location[,] array, string filename)
+        {
+            /// Will save to a .csv file. 
+            /// 1st column) ID, 2) XRef, 3) YRef, 4) TileType, 5) ObjectType.
+            /// Comma acts as the seperator.
+            int z;
+            string line;
+            using (StreamWriter outfile = new StreamWriter(@filename))
+            {
+                for (int x = 0; x < XDimension; x++)
+                {
+                    for (int y = 0; y < YDimension; y++)
+                    {
+                        z = (x * XDimension) + y;
+                        line = array[x,y].ID + "," + array[x, y].XRef + "," + array[x, y].YRef + "," + array[x, y].TileType + "," + array[x, y].ObjectType;
+                        outfile.WriteLine(line);
+                    }
+                }
+            }
+
+        }
+
+        public Location[,] LoadCSV(string filename)
+        {
+            Location[,] LocationArray = new Location[0,0];
+            /// 1st column) ID, 2) XRef, 3) YRef, 4) TileType, 5) ObjectType.
+            string contents = File.ReadAllText(@filename);
+            string[] ContentsArray = contents.Split('\n');
+            int b = ContentsArray.Length;
+            int Dimension = Convert.ToInt32(Math.Round(Math.Sqrt(b))); /// NOTE: Will fail if the grid is not perfect square.
+            LocationArray = new Location[Dimension, Dimension];
+            string[,] TwoDContents = new string[5, b];
+            string[] TempString;
+            string AnotherTempString = "";
+
+            for (int a = 0; a < b - 1; a++)
+            {
+                AnotherTempString = ContentsArray[a];
+                TempString = ContentsArray[a].Split(',');
+                for (int c = 0; c < 5; c++)
+                {
+                    TwoDContents[c, a] = TempString[c];
+                }
+                int id = Int32.Parse(TempString[0]);
+                int xref = Int32.Parse(TempString[1]);
+                int yref = Int32.Parse(TempString[2]);
+                TempString[4] = TempString[4].Replace("\r", ""); 
+                /// \r will appear on the last string.
+
+
+                Location location = new Location(id, xref, yref, TempString[3], TempString[4]);
+                LocationArray[xref, yref] = location;
+            }
+
+            return LocationArray;
+
+        }
+        
 
     }
 }
---
+++ WpfApplication2/Scripts/CSVFunctions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2.Scripts
+{
+    class CSVFunctions
+    {
+        public void SaveCSV(Location[,] array, string filename, int XDimension, int YDimension)
+        {
+            /// Will save to a .csv file. 
+            /// 1st column) ID, 2) XRef, 3) YRef, 4) TileType, 5) ObjectType.
+            /// Comma acts as the seperator.
+            int z;
+            string line;
+            using (StreamWriter outfile = new StreamWriter(@filename))
+            {
+                for (int x = 0; x < XDimension; x++)
+                {
+                    for (int y = 0; y < YDimension; y++)
+                    {
+                        z = (x * XDimension) + y;
+                        line = array[x, y].ID + "," + array[x, y].XRef + "," + array[x, y].YRef + "," + array[x, y].TileType + "," + array[x, y].ObjectType;
+                        outfile.WriteLine(line);
+                    }
+                }
+            }
+
+        }
+
+        public Location[,] LoadCSV(string filename)
+        {
+            Location[,] LocationArray = new Location[0, 0];
+            /// 1st column) ID, 2) XRef, 3) YRef, 4) TileType, 5) ObjectType.
+            string contents = File.ReadAllText(@filename);
+            string[] ContentsArray = contents.Split('\n');
+            int b = ContentsArray.Length;
+            int Dimension = Convert.ToInt32(Math.Round(Math.Sqrt(b))); /// NOTE: Will fail if the grid is not perfect square.
+            LocationArray = new Location[Dimension, Dimension];
+            string[,] TwoDContents = new string[5, b];
+            string[] TempString;
+            string AnotherTempString = "";
+
+            for (int a = 0; a < b - 1; a++)
+            {
+                AnotherTempString = ContentsArray[a];
+                TempString = ContentsArray[a].Split(',');
+                for (int c = 0; c < 5; c++)
+                {
+                    TwoDContents[c, a] = TempString[c];
+                }
+                int id = Int32.Parse(TempString[0]);
+                int xref = Int32.Parse(TempString[1]);
+                int yref = Int32.Parse(TempString[2]);
+                TempString[4] = TempString[4].Replace("\r", "");
+                /// \r will appear on the last string.
+
+
+                Location location = new Location(id, xref, yref, TempString[3], TempString[4]);
+                LocationArray[xref, yref] = location;
+            }
+
+            return LocationArray;
+
+        }
+    }
+}
